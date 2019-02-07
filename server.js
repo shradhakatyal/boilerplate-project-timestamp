0e7997998a98c347c071761959d6a8283eb30cd5
shradhakatyal <skatyal2204@gmail.com>
2019-02-07 14:03:51 +0530

    new route added

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({
+  optionSuccessStatus: 200
+})); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +23,51 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({
+    greeting: 'hello API'
+  });
 });
+
+app.get("/api/timestamp/:date_string", function (req, res) {
+  // Checking if the date is valid
+  let dateString = req.params.date_string;
+  let date;
+  console.log(dateString);
+  // if(!dateString || dateString === "") {
+  //   date = new Date();
+  //   let responseObj = {
+  //     "unix": date.getTime(),
+  //     "utc": date.toUTCString()
+  //   }
+  //   res.send(responseObj);
+  // }
+
+if(dateString.indexOf("-") == -1) {
+    dateString = Number(dateString);
+    date = new Date(dateString);
+    let responseObj = {
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    }
+    res.send(responseObj);
+  }
+
+  if (isNaN(Date.parse(dateString)) == false) {
+    date = new Date(dateString);
+    let responseObj = {
+      "unix": date.getTime(),
+      "utc": date.toUTCString()
+    }
+    res.send(responseObj);
+  } else {
+    res.json({
+      "status": "error",
+      "message": "Invalid date string"
+    })
+  }
+
+});
+
 
 
 
